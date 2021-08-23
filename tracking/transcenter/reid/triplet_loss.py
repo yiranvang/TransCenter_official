cc426b7ebbc412bf346ed00eb5d228184894a2bd
Yihon Xu local <yihong@inrialpes.fr>
2021-08-23 11:23:56 +0200

    update license

--- tracking/transcenter/reid/triplet_loss.py
+++ tracking/transcenter/reid/triplet_loss.py
@@ -1,39 +1,33 @@
-##
 ## TransCenter: Transformers with Dense Queries for Multiple-Object Tracking
 ## Copyright Inria
 ## Year 2021
 ## Contact : yihong.xu@inria.fr
 ##
-## The software TransCenter is provided "as is", for reproducibility purposes only.
-## The user is not authorized to distribute the software TransCenter, modified or not.
-## The user expressly undertakes not to remove, or modify, in any manner, the intellectual property notice attached to the software TransCenter.
-## code modified from
-## (1) Deformable-DETR, https://github.com/fundamentalvision/Deformable-DETR, distributed under Apache License 2.0 2020 fundamentalvision.
-## (2) tracking_wo_bnw, https://github.com/phil-bergmann/tracking_wo_bnw, distributed under GNU General Public License v3.0 2020 Philipp Bergmann, Tim Meinhardt.
-## (3) CenterTrack, https://github.com/xingyizhou/CenterTrack, distributed under MIT Licence 2020 Xingyi Zhou.
-## (4) DCNv2, https://github.com/CharlesShang/DCNv2, distributed under BSD 3-Clause 2019 Charles Shang.
-## (5) correlation_package, https://github.com/NVIDIA/flownet2-pytorch/tree/master/networks/correlation_package, Apache License, Version 2.0 2017 NVIDIA CORPORATION.
-## (6) pytorch-liteflownet, https://github.com/sniklaus/pytorch-liteflownet, GNU General Public License v3.0 Simon Niklaus.
-## (7) LiteFlowNet, https://github.com/twhui/LiteFlowNet, Copyright (c) 2018 Tak-Wai Hui.
-## Below you can find the License associated to this LiteFlowNet software:
-## 
-##     This software and associated documentation files (the "Software"), and the research paper
-##     (LiteFlowNet: A Lightweight Convolutional Neural Network for Optical Flow Estimation) including
-##     but not limited to the figures, and tables (the "Paper") are provided for academic research purposes
-##     only and without any warranty. Any commercial use requires my consent. When using any parts
-##     of the Software or the Paper in your work, please cite the following paper:
-## 
-##     @InProceedings{hui18liteflownet,
-##     author = {Tak-Wai Hui and Xiaoou Tang and Chen Change Loy},
-##     title = {LiteFlowNet: A Lightweight Convolutional Neural Network for Optical Flow Estimation},
-##     booktitle  = {Proceedings of IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
-##     year = {2018},
-##     pages = {8981--8989},
-##     }
-## 
-##     The above copyright notice and this permission notice shall be included in all copies or
-##     substantial portions of the Software.
+## TransCenter is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 
+## TransCenter is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program, TransCenter.  If not, see <http://www.gnu.org/licenses/> and the LICENSE file.
+##
+##
+## TransCenter has code derived from
+## (1) 2020 fundamentalvision.(Apache License 2.0: https://github.com/fundamentalvision/Deformable-DETR)
+## (2) 2020 Philipp Bergmann, Tim Meinhardt. (GNU General Public License v3.0 Licence: https://github.com/phil-bergmann/tracking_wo_bnw)
+## (3) 2020 Facebook. (Apache License Version 2.0: https://github.com/facebookresearch/detr/)
+## (4) 2020 Xingyi Zhou.(MIT License: https://github.com/xingyizhou/CenterTrack)
+##
+## TransCenter uses packages from
+## (1) 2019 Charles Shang. (BSD 3-Clause Licence: https://github.com/CharlesShang/DCNv2)
+## (2) 2017 NVIDIA CORPORATION. (Apache License, Version 2.0: https://github.com/NVIDIA/flownet2-pytorch/tree/master/networks/correlation_package)
+## (3) 2019 Simon Niklaus. (GNU General Public License v3.0: https://github.com/sniklaus/pytorch-liteflownet)
+## (4) 2018 Tak-Wai Hui. (Copyright (c), see details in the LICENSE file: https://github.com/twhui/LiteFlowNet)
 # Adapted from: https://omoindrot.github.io/triplet-loss
 
 ##############
--- tracking/transcenter/util/tracker_util.py
+++ tracking/transcenter/util/tracker_util.py
@@ -1,39 +1,6 @@
-##
-## TransCenter: Transformers with Dense Queries for Multiple-Object Tracking
-## Copyright Inria
-## Year 2021
-## Contact : yihong.xu@inria.fr
-##
-## The software TransCenter is provided "as is", for reproducibility purposes only.
-## The user is not authorized to distribute the software TransCenter, modified or not.
-## The user expressly undertakes not to remove, or modify, in any manner, the intellectual property notice attached to the software TransCenter.
-## code modified from
-## (1) Deformable-DETR, https://github.com/fundamentalvision/Deformable-DETR, distributed under Apache License 2.0 2020 fundamentalvision.
-## (2) tracking_wo_bnw, https://github.com/phil-bergmann/tracking_wo_bnw, distributed under GNU General Public License v3.0 2020 Philipp Bergmann, Tim Meinhardt.
-## (3) CenterTrack, https://github.com/xingyizhou/CenterTrack, distributed under MIT Licence 2020 Xingyi Zhou.
-## (4) DCNv2, https://github.com/CharlesShang/DCNv2, distributed under BSD 3-Clause 2019 Charles Shang.
-## (5) correlation_package, https://github.com/NVIDIA/flownet2-pytorch/tree/master/networks/correlation_package, Apache License, Version 2.0 2017 NVIDIA CORPORATION.
-## (6) pytorch-liteflownet, https://github.com/sniklaus/pytorch-liteflownet, GNU General Public License v3.0 Simon Niklaus.
-## (7) LiteFlowNet, https://github.com/twhui/LiteFlowNet, Copyright (c) 2018 Tak-Wai Hui.
-## Below you can find the License associated to this LiteFlowNet software:
-## 
-##     This software and associated documentation files (the "Software"), and the research paper
-##     (LiteFlowNet: A Lightweight Convolutional Neural Network for Optical Flow Estimation) including
-##     but not limited to the figures, and tables (the "Paper") are provided for academic research purposes
-##     only and without any warranty. Any commercial use requires my consent. When using any parts
-##     of the Software or the Paper in your work, please cite the following paper:
-## 
-##     @InProceedings{hui18liteflownet,
-##     author = {Tak-Wai Hui and Xiaoou Tang and Chen Change Loy},
-##     title = {LiteFlowNet: A Lightweight Convolutional Neural Network for Optical Flow Estimation},
-##     booktitle  = {Proceedings of IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
-##     year = {2018},
-##     pages = {8981--8989},
-##     }
-## 
-##     The above copyright notice and this permission notice shall be included in all copies or
-##     substantial portions of the Software.
-
+#########################################
+# Still ugly file with helper functions #
+#########################################
 
 import os
 from collections import defaultdict
